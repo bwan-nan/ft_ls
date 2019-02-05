70bf03b7950936bafa6eb312c134b3851ad45b52
Cedric M'PASSI <cempassi@e3r1p2.42.fr>
2019-02-05 15:00:45 +0100

    Update xattr

--- srcs/xattr.c
+++ srcs/xattr.c
@@ -6,7 +6,7 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 23:27:45 by cempassi          #+#    #+#             */
-/*   Updated: 2019/02/05 01:27:05 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/02/05 15:00:16 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@ void	print_xattr(t_status *file, char *xattr)
 {
 	int		len;
 	ssize_t	size;
+	int		xattr_pad;
+	int		size_pad;
 
 	if (!file->xattr_len)
 		return ;
+	xattr_pad = 20;
+	size_pad = 10;
 	len = 0;
 	size = getxattr(file->path, &xattr[len], NULL, 0, 0, XATTR_NOFOLLOW);
 	ft_putchar('\n');
-	ft_printf("          %s       %d", xattr, size);	
+	ft_printf("%*s%-*s%*d", 8, "", xattr_pad, xattr, size_pad, size);	
 	len = ft_strlen(&xattr[len]);
 	file->xattr_len -= len + 1;
 	len++;
